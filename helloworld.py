6696368a27eb6cdf27b32af94786618fa94d3f9b
CZX <rayudu.addagarla@economical.com>
2022-11-17 14:34:11 -0500

    Class 1 done

---
+++ helloworld.py
@@ -0,0 +1,49 @@
+"""
+Two ways to comment in python
+1. single #
+2. Three "
+Print my name.
+This is my first python program
+Learn how to indent.
+"""
+
+print("My name is Rayudu")
+
+"""
+Rules:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# variable & assign a value to the variable.
+# Math . Give two variables a,b and then find add them.
+"""
+a = 5
+A = 65
+b = 10
+c = b/a
+name = "John"
+city = "Toronto"
+print(a)
+print(A)
+
+x, y, z = 10, 20, 30
+print(x, y , z, city)
+print("The city name is " + city)
+#casting - converting one data type to another type.
+print("Adding x and y " + str(x+y) )
+
+isTodayCold = False
+if isTodayCold == True:
+    print("its cold today")
+
+# learn data types
+# integer, str, bool, float, lists
+
+#operations
+# + Addition, - Subtraction, * Multiplication / divide % Modulus
+# ** Exponentiation
+k=30
+print(k)
+Street="Broadway"
+print(street)
+
