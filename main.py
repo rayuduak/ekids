a8a5eea82766747df4c3565c3765673809568a5d
CZX <rayudu.addagarla@economical.com>
2022-11-17 14:33:24 -0500

    Class 1 done

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import turtle
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -14,3 +17,20 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+t = turtle.Pen()
+turtle.bgcolor('black')
+turtle.color('yellow')
+turtle.setx(-150)
+turtle.sety(150)
+turtle.write("WELCOME TO EPAM EKIDS", font=('Verdana', 18,'bold'))
+
+for x in range(0,90,2):
+    t.pencolor(colors[x%6])
+    t.width(x/100+1)
+    t.forward(x)
+    t.left(59)
+
+input("waiting...")
